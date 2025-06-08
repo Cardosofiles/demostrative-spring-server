071de6dab46f475d2d999287b5f2d7204479610c
cardosofiles <cardosofiles@outlook.com>
2025-06-08 19:03:30 -0300

    chore: demonstrative-spring application

---
+++ src/main/java/br/com/cardosofiles/demonstrative_spring_server/DemonstrativeSpringServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.cardosofiles.demonstrative_spring_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemonstrativeSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemonstrativeSpringServerApplication.class, args);
+	}
+
+}
