ae64a0782ec2ba6b63378fb2f2303cea2474ae65
cardosofiles <cardosofiles@outlook.com>
2025-06-09 22:51:11 -0300

    chore: working with the request header

--- src/main/java/br/com/cardosofiles/demonstrative_spring_server/controller/Controller.java
+++ src/main/java/br/com/cardosofiles/demonstrative_spring_server/controller/Controller.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,4 +37,20 @@ public class Controller {
         return "Usuário - id: " + user.getId() + " | Nome: " + user.getUser() + " | Telefone: "
                 + user.getTelephone();
     }
+
+    public record UserRecord(String username) {
+    }
+
+    @PostMapping("/headersParams")
+    public String postHeadersParams(@RequestHeader("name") String name) {
+
+        return "Metodo com headerParams: " + name;
+    }
+
+    @PostMapping("/headersParamsList")
+    public String postMethodHeadersParamsList(@RequestBody Map<String, String> headers) {
+        return "Metodo com headerParams: " + headers.entrySet();
+    }
+
+
 }
